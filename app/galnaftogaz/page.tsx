1ff83b90aad3cbe3f801d1194d444c078a24a7a7
Waldemar Tabaczyński <wtabaczynski@caseltech.com>
2025-03-04 03:14:25 +0100

    Galnaftogaz update

--- app/galnaftogaz/page.tsx
+++ app/galnaftogaz/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
@@ -10,7 +10,11 @@ const textVariants = {
 
 const imageVariants = {
   hidden: { opacity: 0, scale: 0.8 },
-  visible: { opacity: 1, scale: 1, transition: { duration: 0.8, ease: "easeOut" } },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 0.8, ease: "easeOut" },
+  },
 };
 
 const buttonVariants = {
@@ -20,24 +24,25 @@ const buttonVariants = {
 
 const galnaftogaz = () => {
   return (
-    <motion.main 
+    <motion.main
       className="flex flex-col items-center justify-center min-h-screen bg-white text-black p-10"
       initial="hidden"
       animate="visible"
     >
-      <motion.section 
+      <motion.section
         className="max-w-6xl flex flex-col md:flex-row items-center gap-10"
         initial="hidden"
         animate="visible"
         variants={textVariants}
       >
         {/* Right side with text and heading */}
-        <motion.div 
+        <motion.div
           className="w-full md:w-1/2 text-black text-lg"
           variants={textVariants}
         >
           <h1 className="text-4xl font-bold mb-6 text-black">
-            Fraud detection for Galnaftogaz
+            Fraud Detection in Transactional Data of the Fishka Loyalty System.
+            Galnaftogaz
           </h1>
 
           <p className="font-bold underline text-xl mb-2">Sector: Retail</p>
@@ -46,21 +51,37 @@ const galnaftogaz = () => {
           <br />
 
           <p>
-          Implementation of a fraud detection project for transactional datasets from the Fishka loyalty system.
-          The project utilized both supervised and unsupervised machine learning algorithms to detect anomalies in sales transactions.
-          Sales transactions flagged as anomalies, when analyzed by the operational team, help identify loopholes in promotional rules or regulations that fraudsters exploit for financial gain.
+            The 'Fraud Detection in Transactional Data of the Fishka Loyalty
+            System' project leverages machine learning algorithms to identify
+            anomalies in sales transactions. Loyalty transactions flagged as
+            suspicious undergo additional verification by the operational team
+            and are subsequently blocked in the system if confirmed as
+            fraudulent. The integration of ML/AI algorithms significantly
+            accelerates the analysis process and helps uncover fraud strategies
+            used by employees or program participants. This enables proactive
+            measures, such as refining program regulations or adjusting business
+            rules, to minimize the risk of fraudulent activities.
           </p>
         </motion.div>
 
         {/* Left side with image and right-aligned button */}
-        <motion.div 
+        <motion.div
           className="flex flex-col items-center md:items-start w-full md:w-1/2"
           variants={textVariants}
         >
           <motion.div variants={imageVariants}>
-            <Image src="/fraud.jpeg" alt="galnaftogaz" width={600} height={350} className="rounded-lg" />
+            <Image
+              src="/fraud.jpeg"
+              alt="galnaftogaz"
+              width={600}
+              height={350}
+              className="rounded-lg"
+            />
           </motion.div>
-          <motion.div className="mt-10 flex justify-end w-full" variants={buttonVariants}>
+          <motion.div
+            className="mt-10 flex justify-end w-full"
+            variants={buttonVariants}
+          >
             <Link href="/#contact-us">
               <button className="px-6 py-3 bg-blue-800 text-white font-bold uppercase rounded-md hover:bg-blue-900 transition">
                 Contact Us
