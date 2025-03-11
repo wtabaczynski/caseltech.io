051db2c72611ced74e63dbcd51b440ca32a3c24c
Waldemar Tabaczyński <wtabaczynski@caseltech.com>
2025-03-11 11:55:11 +0100

    Add privacy policy

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -2,74 +2,9 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 import ContactFormEmail from "@/email/contact-form-email";
 import { validateString, getErrorMessage } from "@/lib/utils";
-// import {
-//   SecretsManagerClient,
-//   GetSecretValueCommand,
-// } from "@aws-sdk/client-secrets-manager";
-
-// const secret_name = "prod/app/website";
-
-// const client = new SecretsManagerClient({
-//   region: process.env.AWS_REGION || "eu-west-1",
-//   credentials: process.env.AWS_ACCESS_KEY_ID
-//     ? {
-//         accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-//         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-//       }
-//     : undefined,
-// });
-
-// Funkcja asynchroniczna do pobierania sekretu z AWS Secrets Manager
-// async function getResendApiKey() {
-//   try {
-//     const response = await client.send(
-//       new GetSecretValueCommand({
-//         SecretId: secret_name,
-//         VersionStage: "AWSCURRENT",
-//       })
-//     );
-
-//     if (!response.SecretString) {
-//       throw new Error("No secret value found");
-//     }
-
-//     // Parsowanie sekretu jako JSON
-//     const secretData = JSON.parse(response.SecretString);
-//     console.log("API Key retrieved:", secretData.RESEND_API_KEY);
-
-//     return secretData.RESEND_API_KEY;
-//   } catch (error) {
-//     console.error("Error fetching secret from AWS:", error);
-//     return null;
-//   }
-// }
-
-// import { EmailTemplate } from '@/components/email-template';
-
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-// export async function POST() {
-//   const { data, error } = await resend.emails.send({
-//     from: "production@resend.dev",
-//     to: "info@caseltech.com",
-//     subject: "New Contact Form Submission",
-//     react: EmailTemplate({ firstName: 'John' }),
-//   });
-
-//   if (error) {
-//     return Response.json({ error });
-//   }
-
-//   return Response.json(data);
-// }
 
 export async function POST(req: Request) {
   try {
-    // const apiKey = await getResendApiKey();
-    // if (!apiKey) {
-    //   return NextResponse.json({ error: "Missing API Key" }, { status: 500 });
-    // }
-
     const resend = new Resend(process.env.RESEND_API_KEY);
 
     // Pobieramy surowy tekst requesta
